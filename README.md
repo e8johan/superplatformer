fcd86766587e6da95913a0005df2315c1b48cd16
Johan Thelin <e8johan@gmail.com>
2019-12-27 17:00:58 +0100

    Enemies stop at edges by default (can be disabled in editor)

--- README.md
+++ README.md
@@ -9,8 +9,7 @@ This is the Super Platformer - a Godot game made by me and my kids.
 
 I want to get the following done (in no particular order):
 
-- Enemies should sense the edge of platforms and not fall off
-- Enimies should kill the player
+- Enemies should kill the player
 - The player should be able to stomp (some) enimies
 - There should be an android soft joystick overlay
 - There should be treasures
--- superplatformer/src/actors/Stompa.gd
+++ superplatformer/src/actors/Stompa.gd
@@ -1,11 +1,25 @@
 extends ActorBase
 class_name Stompa, "res://assets/monsters/stompa/stompa_symbol.png"
 
+export var stop_at_edges : bool = true setget _set_stop_at_edges
+
+onready var groundDetectorRight = $GroundDetectorRight
+onready var groundDetectorLeft = $GroundDetectorLeft
+
 func _ready() -> void:
     _velocity.x = speed.x
+    _set_stop_at_edges(stop_at_edges)
 
 func _physics_process(delta: float) -> void:
     _velocity.y += gravity * delta
-    if is_on_wall():
+    if is_on_wall() or (stop_at_edges and (not groundDetectorRight.is_colliding() or not groundDetectorLeft.is_colliding())):
         _velocity.x = -_velocity.x
     _velocity.y = move_and_slide(_velocity, Vector2.UP).y
+    
+func _set_stop_at_edges(val : bool) -> void:
+    stop_at_edges = val
+    
+    if groundDetectorLeft:
+        groundDetectorLeft.enabled = val
+    if groundDetectorRight:
+        groundDetectorRight.enabled = val
